8711202a53929f411743acbc58810f92c9730312
karthikeyan.V <33289300+karthi-the-hacker@users.noreply.github.com>
2022-12-05 20:22:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const CONST = require('./includes/const');
+const express = require("express");
+const { exec } = require("child_process");
+const app = express();
+var ip = require("ip");
+const cookieParser = require('cookie-parser');
+const lowdb = require('lowdb');
+const bodyParser = require('body-parser');
+const FileSync = require('lowdb/adapters/FileSync'),
+    path = require('path'),
+    adapter = new FileSync('maindb.json'),
+    db = lowdb(adapter);
+
+app.listen(CONST.web);
+
+app.set('view-engine','ejs');
+app.use(cookieParser()); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/webpublic/'));
+app.use(require('./includes/route'));
